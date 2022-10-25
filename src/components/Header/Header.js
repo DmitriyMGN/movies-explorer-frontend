09a122c26c8f9b312194b1f5d45b23c091750e03
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-10-25 19:13:36 +0300

    feat: main start

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,17 @@ function Header (props) {
   const location = useLocation()
   return (
   <header className="header">
-    <img src={mestoLogo} alt="Картинка логотипа сайта" />
     <div className="navbar">
-      {location.pathname === '/' && <Link className="navbar__element" to="/signup">Регистрация</Link> }
+      <img src={mestoLogo} alt="Картинка логотипа сайта" />
+      {location.pathname === '/movies' && <Link className="navbar__element" to="">Фильмы</Link> } 
+      {location.pathname === '/movies' && <Link className="navbar__element" to="">Сохраненные фильмы</Link> }
+    </div> 
+    <div className="navbar">
+      {location.pathname === '/' && <Link className="navbar__element" to="">Регистрация</Link> }
       {location.pathname === '/' && <button onClick={props.signIn} className="navbar__element navbar__element_type_button">Войти</button> }
-      {/* {location.pathname === '/' && <p className="navbar__element">{props.email}</p> } */}
-      {/* {location.pathname === '/h' && <button onClick={props.signOut} className="navbar__element navbar__element_type_button">Выйти</button> } */}
+      {location.pathname === '/movies' && <Link className="navbar__element" to="">Аккаунт</Link> }
+      {location.pathname === '/movies' && <button onClick={props.signIn} className="navbar__element navbar__element_avatar-icon"></button> }
+
     </div>
   </header>
   )
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,22 @@
+import searchLogo from '../../images/search.svg';
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+
+function SearchForm(props) {
+
+  return (
+   <>
+   <div className="searchForm">
+
+    <form className="searchForm__form" onSubmit={props.onRegister}>
+      <img className="searchForm__loop-img" src={searchLogo} alt="Картинка лупы" />
+      <input className="searchForm__input" type="text" placeholder="Фильм"></input>
+      <button className="searchForm__search-button" onClick={props.signIn}></button>
+    </form>
+    <FilterCheckbox />
+   </div>
+  
+   </>
+  );
+}
+
+export default SearchForm;
