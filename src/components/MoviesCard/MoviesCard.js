eaadf7d215108ed616a25d370654da71478f807c
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-10-27 20:59:22 +0300

    feat: movies adaptive

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,75 @@
+import movie from '../../images/movie.png';
+
+function MoviesCard(props) {
+  
+  return (
+    <>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+    <li className="movies__item">
+      <div className="movies__about">
+        <p className="movies__title">В погоне за Бенкси</p>
+        <p className="movies__duration">27 минут</p>
+      </div>
+      <img className="movies__img" src={movie} alt="Фото фильма" ></img>
+      <button className="movies__button">Сохранить</button>
+    </li>
+      </>
+  );
+}
+
+export default MoviesCard;
