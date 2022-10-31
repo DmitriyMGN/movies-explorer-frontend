fb8b4e95ba846b10a99f6390b390b896a7b0d36e
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-10-31 09:00:51 +0300

    second review

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,16 +4,22 @@ function Portfolio(props) {
     <h2 className="portfolio__title">Портфолио</h2>
     <ul className="portfolio__list">
       <li className="portfolio__item">
-        <a className="portfolio__link" href="https://github.com/DmitriyMGN/how-to-learn" target="_blank">Статичный сайт</a>
+        <a className="portfolio__link" href="https://github.com/DmitriyMGN/how-to-learn" target="_blank">
+        Статичный сайт
         <p className="portfolio__link-arrow">↗</p>
+        </a>
       </li>
       <li className="portfolio__item">
-        <a className="portfolio__link" href="https://github.com/DmitriyMGN/russian-travel" target="_blank">Адаптивный сайт</a>
+        <a className="portfolio__link" href="https://github.com/DmitriyMGN/russian-travel" target="_blank">
+        Адаптивный сайт
         <p className="portfolio__link-arrow">↗</p>
+        </a>
       </li>
       <li className="portfolio__item">
-        <a className="portfolio__link" href="https://github.com/DmitriyMGN/react-mesto-api-full" target="_blank">Одностраничное приложение</a>
+        <a className="portfolio__link" href="https://github.com/DmitriyMGN/react-mesto-api-full" target="_blank">
+        Одностраничное приложение
         <p className="portfolio__link-arrow">↗</p>
+        </a>
       </li>
     </ul>
    </div>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm(props) {
    <div className="searchForm">
     <form className="searchForm__form" onSubmit={props.onRegister}>
       <img className="searchForm__loop-img" src={searchLogo} alt="Картинка лупы" />
-      <input className="searchForm__input" type="text" placeholder="Фильм"></input>
+      <input className="searchForm__input" type="text" placeholder="Фильм" required></input>
       <button className="searchForm__search-button" onClick={props.signIn}></button>
     </form>
     <FilterCheckbox />
