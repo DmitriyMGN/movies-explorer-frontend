3c8097d1ac9a28618c64701588959bad23328216
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-11-10 08:33:46 +0300

    review 1

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-function Error (props) {
+function Error () {
+  const history = useHistory();
+
+  const handlerGoBack = () => history.go(-1)
+  
   return (
   <section className="error">
       <h2 className="error__title">404</h2>
       <p className="error__subtitle">Страница не найдена</p>
-      <Link className='error__exit-link' to="/">Назад</Link>
+      <Link className='error__exit-link' onClick={handlerGoBack}>Назад</Link>
   </section>
   )
 }
